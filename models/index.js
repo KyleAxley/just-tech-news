86ec50e00d4f1e4678069f185e8899d878c66534
KyleAxley <kanderson7645@yahoo.com>
2022-08-09 13:41:34 -0400

    CRUD methods finished for posts

--- models/index.js
+++ models/index.js
@@ -2,6 +2,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+//create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
-//export object
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+//export User, Post object
 module.exports = { User, Post };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+//import for user and post routes
 const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
+//call to use user and post routes
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
