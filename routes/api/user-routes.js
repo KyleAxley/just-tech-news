2524c27e75b8b0d209fba0f68c819983e012f3a5
KyleAxley <kanderson7645@yahoo.com>
2022-08-09 10:24:26 -0400

    typo fixed

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -73,7 +73,7 @@ router.post('/login', (req, res) => {
       res.status(400).json({ message: 'Incorrect password! '});
       return;
     }
-    res.json({ user: dbUserData, message: 'You are not logged in!' });
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
 });
 
