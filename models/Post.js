f311b8a6e7df711de3eb39fd242971d69e4e9f1d
KyleAxley <kanderson7645@yahoo.com>
2022-08-13 12:44:39 -0400

    votes routes added

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,32 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// create our Post model
+class Post extends Model {
+  static upvote(body, models) {
+    return models.Vote.create({
+      user_id: body.user_id,
+      post_id: body.post_id
+    }).then(() => {
+      return Post.findOne({
+        where: {
+          id: body.post_id
+        },
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [
+            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+            'vote_count'
+          ]
+        ]
+      });
+    });
+  }
+}
 
-//create our Post model(table)
-class Post extends Model {}
-
+// create fields/columns for Post model
 Post.init(
   {
     id: {
@@ -21,21 +44,21 @@ Post.init(
       allowNull: false,
       validate: {
         isURL: true
-      },
+      }
     },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "user",
-        key: "id"
-      },
-    },
+        model: 'user',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: "post"
+    modelName: 'post'
   }
 );
 
--- models/index.js
+++ models/index.js
@@ -1,15 +1,36 @@
-//import User, Post model
+//import User, Post, Vote model
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
-//create associations
-User.hasMany(Post, {
+//create associations between tables using foreign key 'user_id'
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id',
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
     foreignKey: 'user_id'
 });
 
-Post.belongsTo(User, {
+Vote.belongsTo(User, {
     foreignKey: 'user_id'
 });
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
 
 //export User, Post object
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
